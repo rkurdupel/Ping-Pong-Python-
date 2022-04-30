f1753831a749a14e07ad9e0227c775d19785eba2
Roman Kurdupel <87699645+rkurdupel@users.noreply.github.com>
2022-04-30 16:35:00 +0200

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -1,48 +1,47 @@
 import pygame
-pygame init ()
+pygame.init()
 
 
 width = 600
 height = 500
 
-class GameSprite(sprite.Sprite):
+class GameSprite(pygame.sprite.Sprite):
   def __init__(self, player_image, player_x, player_y, player_speed, width, height):
     super().__init__()
-    self.image = transform.scale(image.load(player_image))
+    self.image = pygame.transform.scale(pygame.image.load(player_image))
     self.speed = player_speed
     self.rect = self.image.get_rect()
     self.rect.x = player_x
     self.rect.y = player_y
     
- def reset(self):
-  window.blit(self.image, (self.rect.x, self.rect.y)
+def reset(self):
+    pygame.window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-  def update_right(self):
-    keys = key.get_pressed()
-    if keys[K_UP] and self.rect.y > 5:
-      self.rect.y -= self.speed
-    if keys[K_DOWN] and self.rect.y > 420:
-      self.rect.y += self.speed
+    def update_right(self):
+        keys = pygame.key.get_pressed()
+        if pygame.keys[pygame.K_UP] and pygame.self.rect.y > 5:
+          pygame.self.rect.y -= pygame.self.speed
+        if pygame.keys[pygame.K_DOWN] and pygame.self.rect.y > 420:
+          pygame.self.rect.y += pygame.self.speed
 
 
-  def update_left(self):
-    if keys[K_w] and self.rect.y > 5:
-      self.rect.y -= self.speed
+    def update_left(self):
+      if pygame.keys[pygame.K_w] and self.rect.y > 5:
+        self.rect.y -= self.speed
 
-    if keys[K_s] and self.rect.y > 420:
-      self.rect.y += self.speed
+      if pygame.keys[pygame.K_s] and self.rect.y > 420:
+        self.rect.y += self.speed
 
-   
-          
-              
-window = pygame.display.set_mode((win_width, win_height))
 run = True
+finish = False
 
-While run:
-  run = True
+window = pygame.display.set_mode((width, height))
+
+while run:
   for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-              run = False
-  pygame.display.update()
-  
+    if event.type == pygame.QUIT:
+      run = False
+
+    pygame.display.update()
+
